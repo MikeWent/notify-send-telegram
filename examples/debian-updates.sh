5c255ec271624ddf55f09fb52e6168ab5890f243
Mike_Went <git@meew.me>
2017-11-15 01:07:19 +0300

    Add debian-updates.sh to examples

---
+++ examples/debian-updates.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##
+# Notify Send Telegram (github.com/MikeWent/notify-send-telegram)
+# Released under the MIT license
+#
+# This script is just an example of use
+##
+
+# one or zero
+notify_even_when_updated=1
+
+echo "Updating apt cache..."
+status=$(sudo apt update | tail -n 1 | cut -d "." -f1)
+if [ "$status" == "All packages are up to date" ]
+	then
+	if [ $notify_even_when_updated -eq 1 ]
+		then
+		nst "$status"
+	fi
+else
+	nst "Updates available" "$status"
+fi
+echo "Done"
+
