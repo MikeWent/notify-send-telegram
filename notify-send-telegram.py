92d4a83da060e92a073494956fe14844945413ba
Mike_Went <git@meew.me>
2017-11-14 22:48:18 +0300

    Impove code formatting

--- notify-send-telegram.py
+++ notify-send-telegram.py
@@ -42,11 +42,8 @@ if __name__ == "__main__":
     from os.path import expanduser
     from os import makedirs
     import argparse
-    """
-        Args parser
-    """
-    parser = argparse.ArgumentParser(usage="%(prog)s [options] SUMMARY [BODY]")
     
+    parser = argparse.ArgumentParser(usage="%(prog)s [options] SUMMARY [BODY]")
     parser.add_argument("SUMMARY", help="Notification title, for example 'New mail'")
     parser.add_argument("BODY", help="Text of notification, multi-line supported. If BODY isn't set, just the SUMMARY is sent", default="", nargs="?")
     parser.add_argument("-n", "--silent", help="send notification with no sound", default=False, action="store_true")
@@ -57,13 +54,11 @@ if __name__ == "__main__":
     options = parser.parse_args()
 
     config_full_path = expanduser(CONFIG_PATH) + CONFIG_FILE
-
     config = configparser.ConfigParser()
     config.read(config_full_path)
     if len(config.sections()) == 0:
-        # if empty config
-        config["default"] = {"token": "",
-                             "recipient": ""}
+        # default values if config was empty
+        config["default"] = {"token": "", "recipient": ""}
         config_was_empty = True
     else:
         config_was_empty = False
@@ -94,10 +89,13 @@ if __name__ == "__main__":
 
     # init bot
     bot = Telegram(token_to_use)
-    # construct message
+
+    # escape html tags
     summary = html.escape(options.SUMMARY).replace("\\n", "\n")
     body = html.escape(body).replace("\\n", "\n")
+    # construct message
     text = "<b>"+summary+"</b>\n"+body
+    
     request_result = bot.send_message(recipient_to_use, text, options.silent)
     if not request_result["ok"]:
         print("Telegram API returned an error!")
